e38e4a3d2d7389b0c3a6b1b501390e886652ccbf
joachimb08 <joachimb08.kodehode@gmail.com>
2023-08-31 10:23:04 +0200

    update

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function myFunction() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
+function myFunction() {
+  const button = (document.getElementById(
+    "btn"
+  ).innerText = "Light Mode");
+}
